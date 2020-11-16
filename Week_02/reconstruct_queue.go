00f239a00f0a8097ce3d10061dd462b397584078
liyuan22 <liyuan22@bj-m-217448a.local>
2020-11-16 19:39:36 +0800

    update w2

---
+++ Week_02/reconstruct_queue.go
@@ -0,0 +1,33 @@
+package w3
+
+import (
+	"sort"
+)
+
+//
+func ReconstructQueue(people [][]int) [][]int {
+	sort.Slice(people,func(i,j int )bool{
+		if people[i][0]==people[j][0]{
+			return people[i][1]<people[j][1]
+		}
+		return people[i][0]>people[j][0]
+	})
+	for i,v:=range people{
+		copy(people[v[1]+1:i+1],people[v[1]:i])
+		people[v[1]]=v
+	}
+	return people
+}
+
+func ReconstructQueue1(people [][]int) [][]int {
+	sort.Slice(people, func(i, j int) bool {
+		return people[i][0]>people[j][0]||
+			(people[i][0]==people[j][0]&&people[i][1]<people[j][1])
+	})
+	for i,v:=range people{
+		indexV:=v[1]
+		copy(people[indexV+1:i+1],people[indexV:i])
+		people[v[1]]=v
+	}
+	return people
+}
