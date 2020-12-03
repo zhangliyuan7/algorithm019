a27cff6c2eb08eb23ac873df3aa18fdaec2e14c7
liyuan22 <liyuan22@bj-m-217448a.local>
2020-12-03 19:08:46 +0800

    update

---
+++ Week_06/max_sub_array.go
@@ -0,0 +1,23 @@
+package Week_06
+
+
+
+//动态规划 只要往前累计比当前大，那么把之前的和加到当前来
+func MaxSubArray(nums []int) int {
+	var max int = nums[0]
+	for i:=1;i<len(nums);i++{
+		if nums[i]+nums[i-1]>nums[i]{
+			nums[i]=nums[i-1]+nums[i]
+		}
+		if nums[i]>max{
+			max=nums[i]
+		}
+	}
+	return max
+}
+//func  MaxSubArray2(nums []int) int {
+//	var max int = nums[0]
+//	for i:=1;i<len(nums);i++{
+//		if max<
+//	}
+//}
---
+++ Week_06/max_sub_array_test.go
@@ -0,0 +1,10 @@
+package Week_06
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestMaxSubArray(t *testing.T) {
+	fmt.Println(MaxSubArray([]int{-2,1,-3,4,-1,2,1,-5,4}))
+}
