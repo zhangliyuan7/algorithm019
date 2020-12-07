5cf0bf7f82b53dc9de71a9857057728d9b633cff
liyuan22 <liyuan22@bj-m-217448a.local>
2020-12-07 11:46:47 +0800

    d1w6

---
+++ Week_06/unique_path_test.go
@@ -0,0 +1,10 @@
+package Week_06
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestUniquePaths(t *testing.T) {
+	fmt.Println(UniquePaths(1,2))
+}
---
+++ Week_06/unique_paths.go
@@ -0,0 +1,25 @@
+package Week_06
+
+func UniquePaths(m int, n int) int {
+	if m==n&&m==1{
+		return 1
+	}
+	var dp =make([][]int,m)
+	for i:=range dp{
+		dp[i]=make([]int,n)
+	}
+	dp[0][0]=0
+
+	for i:=1;i<m;i++{
+		dp[i][0]=1
+	}
+	for j:=1;j<n;j++{
+		dp[0][j]=1
+	}
+	for i:=1;i<m;i++{
+		for j:=1;j<n;j++{
+			dp[i][j]=dp[i-1][j]+dp[i][j-1]
+		}
+	}
+	return dp[m-1][n-1]
+}
