ad92b554f58d4055e2cc752558cc3aced332fbab
liyuan22 <liyuan22@bj-m-217448a.local>
2021-04-08 15:30:40 +0800

    daily practice

---
+++ Repeat_01/find_min.go
@@ -0,0 +1,45 @@
+package Repeat_01
+
+//153
+func findMin(nums []int) int {
+	if len(nums)==1{
+		return nums[0]
+	}
+	if len(nums)==0{
+		return -1
+	}
+	l:=0
+	r:=len(nums)-1
+	if nums[l]<nums[r]{
+		return nums[l]
+	}
+	if nums[r]<nums[l]&&nums[r]<nums[r-1]{
+		return nums[r]
+	}
+	for l<r{
+		mid:=l+(r-l)/2
+		if nums[mid]<nums[mid-1]&&nums[mid]<nums[mid+1]{
+			return nums[mid]
+		}
+		if nums[mid]>nums[r]{
+			l=mid
+		}
+		if nums[mid]<nums[l]{
+			r=mid
+		}
+	}
+	return -1
+}
+
+func findMinI(nums []int) int {
+	l,r:=0,len(nums)-1
+	for l<r{
+		mid:=l+(r-l)/2
+		if nums[mid]<nums[r]{
+			r=mid
+		}else{
+			l=mid+1
+		}
+	}
+	return nums[l]
+}
