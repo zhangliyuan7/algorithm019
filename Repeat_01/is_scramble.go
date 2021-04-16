1bdcf3b76550ca3574b4b5973b6ee66136b31e9e
liyuan22 <liyuan22@bj-m-217448a.local>
2021-04-16 17:12:44 +0800

    update isSrcamble hashCode

--- Repeat_01/is_scramble.go
+++ Repeat_01/is_scramble.go
@@ -1,27 +1,27 @@
 package Repeat_01
 
 // 87
-var memo map[string]bool
+var mem map[string]bool
 func isScramble(s1 string, s2 string) bool {
-	memo = make(map[string]bool)
+	mem = make(map[string]bool)
 	return dfsIsScramble(s1,s2)
 }
 func dfsIsScramble(s1 ,s2 string )bool{
 	key :=s1+s2
-	if value, ok := memo[key]; ok {
+	if value, ok := mem[key]; ok {
 		return value
 	}
 	if s1==""||s2==""||len(s1)!=len(s2){
-		memo[key]=false
+		mem[key]=false
 		return false
 	}
 	if s1==s2{
-		memo[key]=true
+		mem[key]=true
 		return true
 	}
 	//判断包含的字母是否相同
 	if hashCode(s1)!=hashCode(s2){
-		memo[key]=false
+		mem[key]=false
 		return false
 	}
 	// m:=make(map[string]int)
@@ -38,19 +38,19 @@ func dfsIsScramble(s1 ,s2 string )bool{
 	ln:=len(s1)
 	for i:=1;i<ln;i++ {
 		if dfsIsScramble(s1[:i], s2[:i]) && dfsIsScramble(s1[i:], s2[i:]) {
-			memo[key]=true
+			mem[key]=true
 			return true
 		}
 		//dfs(s1[:i], s2[l1-i:]) && dfs(s1[i:], s2[:l1-i])
 		if dfsIsScramble(s1[:i], s2[ln-i:]) && dfsIsScramble(s1[i:], s2[:ln-i]) {
-			memo[key]=true
+			mem[key]=true
 			return true
 		}
 	}
-	memo[key]=false
+	mem[key]=false
 	return false
 }
-
+// 这是个好方法 单靠这个方法就节省了4ms
 func hashCode(s string )int{
 	x:=0
 	for i:=range s{
