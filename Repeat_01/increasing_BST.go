f2e50232341944b26a36159f52f40732da1a5366
liyuan22 <liyuan22@bj-m-217448a.local>
2021-04-25 11:33:20 +0800

    increasing bst

---
+++ Repeat_01/increasing_BST.go
@@ -0,0 +1,21 @@
+package Repeat_01
+
+
+//897
+func increasingBST(root *TreeNode) *TreeNode {
+	newTree:=new(TreeNode)
+	base:=newTree
+	var midOrderBST func(*TreeNode)
+	midOrderBST=func(root *TreeNode){
+		if root==nil{
+			return
+		}
+		midOrderBST(root.Left)
+		newTree.Right=root
+		root.Left=nil
+		newTree=root
+		midOrderBST(root.Right)
+	}
+	midOrderBST(root)
+	return base.Right
+}
